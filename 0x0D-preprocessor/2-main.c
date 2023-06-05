79934f1543b77ad83c664bc41253a77740af6082
toxicfrank <ngugifrancis930@gmai.com>
2023-06-05 08:33:55 +0300

    practice

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file that it was compiled from
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
