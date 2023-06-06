ae43fc503ce20244fc1f03443b98d586fbb43f29
toxicfrank <ngugifrancis930@gmai.com>
2023-06-06 10:17:33 +0300

    practice

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: function parameter that points to struct dog
+ * Return: NULL during failure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+	{
+		d->name = malloc(sizeof(char *) * strlen("(nil)") + 1);
+		strcpy(d->name, "(nil)");
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
