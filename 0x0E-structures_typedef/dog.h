3896186db1f49d3fc4eef4ead9f49036f9ebd5f9
toxicfrank <ngugifrancis930@gmai.com>
2023-06-06 06:23:22 +0300

    practice

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - short desctiption
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
