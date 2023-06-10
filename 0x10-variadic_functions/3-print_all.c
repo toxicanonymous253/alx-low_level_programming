5c50d1941f2a627774015788cd666530d5a57fd5
toxicfrank <ngugifrancis930@gmai.com>
2023-06-10 11:29:01 +0300

    practice

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: function parameter
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	
+}
