2568ea1d13cc45c07a9d6e63add6da20c870ee59
toxicfrank <ngugifrancis930@gmai.com>
2023-06-14 12:16:01 +0300

    practice

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,5 +8,49 @@
 
 void print_all(const char * const format, ...)
 {
-	
+	int format_specifier;
+	int index = 0;
+	int format_length = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	while	(format[format_length] != '\0')
+		format_length++;
+
+	while (index < format_length)
+	{
+		format_specifier = format[index];
+		index++;
+
+		if (format_specifier == 'c')
+		{
+			int c = va_arg(args, int);
+			printf("%c, ", c);
+		}
+		else if (format_specifier == 'i')
+		{
+			int i = va_arg(args, int);
+			printf("%d, ", i);
+		}
+		else if (format_specifier == 'f')
+		{
+			float f = va_arg (args, double);
+			printf("%f, ", f);
+		}
+		else if (format_specifier == 's')
+		{
+			char *s = va_arg(args, char *);
+
+			if (s == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+				printf("%s", s);
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
