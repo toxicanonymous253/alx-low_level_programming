613c5f9c25c6bdc50bceab427d2ba2ed8ff979e6
toxicfrank <ngugifrancis930@gmai.com>
2023-06-16 21:21:18 +0300

    practice

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,5 +8,23 @@
 
 size_t print_list(const list_t *h)
 {
-	
+	int count = 0;
+
+	if (h ==NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len,h->str);
+		}
+		else{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,5 +21,4 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 
-
 #endif
