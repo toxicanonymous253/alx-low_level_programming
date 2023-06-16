d188dbd4da220d6ef83b951437ad02f64d00e967
toxicfrank <ngugifrancis930@gmai.com>
2023-06-16 21:33:36 +0300

    practice

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list
+ * @head: function parameter
+ * @str: function parameter
+ * Return: the address of the new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	
+}
