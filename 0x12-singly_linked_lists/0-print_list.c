f251e5225fa12a33560d60f4a4f2df641aefeb23
toxicfrank <ngugifrancis930@gmai.com>
2023-06-16 20:42:49 +0300

    practice

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements in a list
+ * @h: function prototype
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	
+}
