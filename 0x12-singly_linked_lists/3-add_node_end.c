e93b5742504e9c6bb961420aabd7c105b27878b7
toxicfrank <ngugifrancis930@gmai.com>
2023-06-17 10:18:08 +0300

    practice

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,5 +9,38 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node;
+	list_t *current = *head;
+	unsigned int len = 0;
 
+	while (str[len])
+	{
+		/* code */
+		len++;
+	}
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+
+		return (new_node);
+	}
+
+	while (current != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+	return (new_node);
 }
