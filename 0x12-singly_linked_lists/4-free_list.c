9e20eb4928c8f1b65dd724b6b4d965d9bb1494df
toxicfrank <ngugifrancis930@gmai.com>
2023-06-17 10:36:16 +0300

    pracitce

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,20 @@
 #include "lists.h"
 
 /**
- * 
+ * free_list - function that frees a list
+ * @head: function parameter
+ * Return: void
  */
 
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current;
+
+	while (current != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
