8793b349b7f8be7adbc36ebe348e89de3b67343c
toxicfrank <ngugifrancis930@gmai.com>
2023-06-17 07:59:28 +0300

    pracitce

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,32 +1,35 @@
 #include "lists.h"
 
-/**
- * add_node - function that adds a new node at the beginning of a list
- * @head: function parameter
- * @str: function parameter
- * Return: the address of the new element or NULL if it failed
- */
+int _strlen_recursion(char *s);
 
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node = (list_t *)malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->str = strdup(str);
 
-	if (new_node == NULL)
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	new_node->len = strlen(new_node->str);
 
-	new_node = *head;
+	new_node->next = *head;
 
 	*head = new_node;
 
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function that adds a new node at the end of a list
+ * @head: function parameter
+ * @str: function parameter
+ * Return: the addres of the new element or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
