7cf65342273e84f04c8142ec4c266fa214b63a67
toxicfrank <ngugifrancis930@gmai.com>
2023-06-17 07:41:51 +0300

    practice

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,5 +9,26 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	
+	list_t *new_node;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+
+	new_node = *head;
+
+	*head = new_node;
+
+	return (new_node);
 }
