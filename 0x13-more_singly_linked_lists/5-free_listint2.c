81b0c2483a93b8e78b8958aaa56b8cf5213bc38f
toxicfrank <ngugifrancis930@gmai.com>
2023-06-20 12:14:20 +0300

    practice

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-	current = *head;
+	*head = NULL;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
+		current = *head;
 		current = current->next;
 		free(current);
 	}
