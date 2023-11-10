c40f36e352dd754f9196b8d92102dd2ee50d81d2
toxicfrank <ngugifrancis930@gmai.com>
2023-11-10 12:21:39 +0300

    alx

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,19 +7,13 @@
 #include "lists.h"
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *temp;
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
 
-	temp = h;
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
