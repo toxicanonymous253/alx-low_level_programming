340f3ffe88688eebacb686a965a498d1ec5ed14b
toxicfrank <ngugifrancis930@gmai.com>
2023-11-10 13:27:13 +0300

    alx

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,16 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function that frees the list
+ * @head: the head of the linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
 
 	temp = head;
 
-	while(head != NULL)
+	while (head != NULL)
 	{
 		temp = temp->next;
 		free(head);
