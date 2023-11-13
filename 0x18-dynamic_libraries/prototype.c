f65a79ab21c9dd9d4f0a895b9d1ec4e4177086ff
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-11-13 05:22:51 -0800

    alx

---
+++ 0x18-dynamic_libraries/prototype.c
@@ -0,0 +1,103 @@
+#include "main.h"
+
+int _islower(int c)
+{
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	return (0);
+}
+
+int _abs(int n)
+{
+	return (0);
+}
+
+int _isupper(int c)
+{
+	return (0);
+}
+
+int _isdigit(int c)
+{
+	return (0);
+}
+
+int _strlen(char *s)
+{
+	int count  = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+void _puts(char *s)
+{
+	return;
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	return (NULL);
+}
+
+int _atoi(char *s)
+{
+	return (0);
+}
+
+char *_strcat(char *dest, char *src)
+{
+	return (NULL);
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (NULL);
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (NULL);
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (NULL);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (NULL);
+}
+
+char *_strchr(char *s, char c)
+{
+	return (NULL);
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (NULL);
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (NULL);
+}
