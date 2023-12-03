5b66b5aedc4cc3e9a75de3ad9066e0eb98863b90
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-12-03 19:56:28 +0300

    alx

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,14 +11,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 	hash_table_t *table;
 
+	if (!size)
+	{
+		return (NULL);
+	}
+
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	/*Checking if memory has been allocated*/
 	if (table == NULL)
-	{
-		return (NULL);
-	}
-	if (!size)
 	{
 		return (NULL);
 	}
