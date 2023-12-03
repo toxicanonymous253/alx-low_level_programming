a48e4b9af3c774828593fc9baa9fc051a2c25fff
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-12-03 19:53:57 +0300

    alx

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -function willl create the hash table
+ * @size: size of the hash table to be created
+ * Return: table created
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_table_t *table;
+
+	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+
+	/*Checking if memory has been allocated*/
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	if (!size)
+	{
+		return (NULL);
+	}
+
+
+	table->size = size;
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+
+	for (i = 0; i < table->size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
