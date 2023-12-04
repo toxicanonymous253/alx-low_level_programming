ae44c82b31c08e393b90c0767a6af30b3ed66983
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-12-04 11:35:05 +0300

    alx

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,56 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds and element to the hash table
+ * @ht: hash table that you want to add or update the key/value to
+ * @key: this is the key and cannot be empty string
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+/*In case of collision, add the new node at the beginning of the list*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *item = (hash_node_t *) malloc(sizeof(hash_node_t));
+	hash_node_t *curr_item = ht->array[index];
+
+	if (key == NULL)
+	{
+		return (0);
+	}
+	/*Allocating memory for the item*/
+
+	if (item == NULL)
+	{
+		return (0);
+	}
+	item->key = strdup(key); /*make a copy of key*/
+
+	if (item->key == NULL)
+	{
+		return (0);
+	}
+	item->value = strdup(value); /*make a copy of value*/
+	item->next = NULL;
+
+	if (index >= ht->size)
+	{
+		free(item->key);
+		free(item->value);
+		free(item);
+		return (0);
+	}
+
+	if (curr_item == NULL)
+	{
+		ht->array[index] = item;
+		return (1);
+	}
+	/*There is a value in the array*/
+	else
+	{
+		item->next = curr_item;
+		ht->array[index] = item;
+		return (1);
+	}
+}
